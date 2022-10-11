19a2ae12f194b0254ff7c4946ac968b1e3709edc
kyeshmz <kyeshimizu@gmail.com>
2022-10-11 22:16:15 +0900

    add webserver

--- Assets/Cube1Camera.cs
+++ Assets/Cube1Camera.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class Cube1Camera : MonoBehaviour
 {
 
-    public Transform mainCamera;
-    public GameObject player;
-    void Start()
+   public Transform cameraPosition;
+   
+    private void Update()
+    {
+        
+        transform.position = new Vector3(cameraPosition.position.x, cameraPosition.position.y, 0);
+        // transform.localEulerAngles = new Vector3(cameraPosition.rotation.x,0,0);
+        // transform.LookAt(cameraPosition.position.x, cameraPosition.position.y, 0);
+        transform.rotation = cameraPosition.rotation;
+        // transform.rotation = Quaternion.Euler(cameraPosition.rotation.x,0,0);
+        transform.rotation = Quaternion.Euler(0,cameraPosition.rotation.y *45 ,0);
 
-    {
-        mainCamera = Camera.main.transform.parent.transform;
-    }
-    void Update()
-    {
-        transform.position = new Vector3(mainCamera.transform.position.x, mainCamera.transform.position.y, 0);
+        // transform.rotation = new Vector3(transform.rotation.x, cameraPosition.rotation.y, transform.rotation.z);
     }
 }

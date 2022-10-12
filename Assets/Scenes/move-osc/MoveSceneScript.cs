cab3be5f12f8fae81041efa83a950d8fe9f9b2c0
kyeshmz <kyeshimizu@gmail.com>
2022-10-13 00:02:51 +0900

    fix move scene

--- Assets/Scenes/move-osc/MoveSceneScript.cs
+++ Assets/Scenes/move-osc/MoveSceneScript.cs
@@ -7,8 +7,8 @@ public class MoveSceneScript : MonoBehaviour
 {
 
     float intervalTime = 0.05f;
-        float elapsedTime = 0;
-        Cube cube;
+    float elapsedTime = 0;
+    Cube cube;
 
          [SerializeField] int port = 4000;
     OscServer server;
@@ -20,26 +20,19 @@ public class MoveSceneScript : MonoBehaviour
     private int toio1Left = 0;
     private int toio1Right = 0;
     private int toio1Speed = 0;
+    private int toio1posX = 0;
+    private int toio1posY = 0;
 
     private int toio2Left = 0;
     private int toio2Right = 0;
     private int toio2Speed = 0;
+    private int toio2posX = 0;
+    private int toio2posY = 0;
 
     void OnEnable()
     {
         server = new OscServer(port);
-        server.MessageDispatcher.AddCallback(
-            "/test/recieve",
-            (string address, OscDataHandle data) => {
-                var stringValue = data.GetElementAsInt(0);
-                var floatValue = data.GetElementAsFloat(1);
-                var intValue = data.GetElementAsInt(2);
-                Debug.Log($"OscJack receive: {address} {stringValue} {floatValue} {intValue}");
-                 // Cube変数の生成が完了するまで早期リターン
-                 speed = stringValue;
-
-            }
-       );
+       
 
         server.MessageDispatcher.AddCallback(
             "/toio/1",
@@ -103,7 +96,7 @@ public class MoveSceneScript : MonoBehaviour
           // CubeManagerからモジュールを間接利用した場合:
         cubeManager = new CubeManager(ConnectType.Simulator);
         await cubeManager.MultiConnect(2);
-        initToStartPosition();
+     
         //initToResetPosition();
     }
 
@@ -112,64 +105,57 @@ public class MoveSceneScript : MonoBehaviour
     {
         // Cube変数の生成が完了するまで早期リターン
 
+        toio1posX = cubeManager.cubes[0].x;
+        toio1posY = cubeManager.cubes[0].y;
+
+        toio2posX = cubeManager.cubes[1].x;
+        toio2posY = cubeManager.cubes[1].y;
+
         if (toio1Left != 0 && toio1Right != 0 && toio1Speed != 0)
         {
-            cubeManager.cubes[0].Move(toio1Left, toio1Right, toio1Speed);
+            //
+            if (toio1posY < 307 || toio1posY > 414)
+            {
+
+                cubeManager.cubes[0].Move(toio1Left, toio1Right, (int)(toio1Speed* 0.8));
+
+            } else
+            {
+                cubeManager.cubes[0].Move(toio1Left, toio1Right, toio1Speed);
+
+            }
             resetToio1Flags();
         }
 
 
         if (toio2Left != 0 && toio2Right != 0 && toio2Speed != 0)
         {
-            cubeManager.cubes[1].Move(toio2Left, toio2Right, toio2Speed);
+            if (toio2posY < 307 || toio2posY > 414)
+            {
+
+                cubeManager.cubes[1].Move(toio2Left, toio2Right, (int)(toio2Speed * 0.8));
+
+            }
+            else
+            {
+                cubeManager.cubes[1].Move(toio2Left, toio2Right, toio1Speed);
+
+            }
             resetToio2Flags();
         }
+
+
         if (resetFlg)
         {
             //reset
+
 
             resetFlg = false;
         }
 
 
-        //if (speed != 0 ){
-        //        cubeManager.cubes[0].Move(speed, speed, 200);
-        //         cubeManager.cubes[1].Move( -speed, -speed, 200);
-        
-        ////          foreach(var cube in cubeManager.cubes)
-        //// {
-        ////     if (cubeManager.IsControllable(cube))
-        ////     {
-        ////                  cube.Move(speed, speed, 200);
-                          
-
-        ////     }
-        //// }
-        //   speed = 0;
-        
-        //    }
-        //mainCamera.transform.position = new Vector3(cubeManager.cubes[0]., cubeManager.cubes[0].pos.y, 0);
     }
 
-    void initToStartPosition(){
-        //-0.5048941
-        //3.107823e-06
-        //-0.001395815
-        //rot
-        //-0.013
-        //93.046
-        //0.013
-       
-        cubeManager.cubes[0].TargetMove(113, 360, 0,0,0,Cube.TargetMoveType.RoundBeforeMove);
-        cubeManager.cubes[1].TargetMove(870,360,180,0,0, Cube.TargetMoveType.RoundBeforeMove);
-       
-    }
-
-    void initToResetPosition()
-    {
-        cubeManager.cubes[0].TargetMove(492, 307, 0, 0, 0, Cube.TargetMoveType.RoundBeforeMove);
-        cubeManager.cubes[1].TargetMove(492, 414, 180, 0, 0, Cube.TargetMoveType.RoundBeforeMove);
-    }
 
     void resetToio1Flags()
     {
